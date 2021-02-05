2406cac7643d520fef0e68962b7ce43318b1e2b4
NikkiE-Holberton <2495@holbertonschool.com>
2021-02-05 21:01:41 +0000

    Updating size file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,10 @@
 */
 int main(void)
 {
-char = 1
-int = 2
-long int = 3
-long long int = 4
-float = 5
-
-printf("Size of a char: %c byte(s)\n");
-printf("Size of a int: %d byte(s)\n");
-printf("Size of a long int: %li byte(s)\n");
-printf("Size of a long long int: %lli byte(s)\n");
-printf("Size of a float: %f byte(s)\n");
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of a int: %lu byte(s)\n)", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lli byte(s)\n", sizeof(long long int));
+printf("Size of a float: %f byte(s)\n", sizeof(float));
 return (0);
 }
