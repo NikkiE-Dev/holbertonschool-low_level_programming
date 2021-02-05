0fdadf56475c3c9771e29186b222ead9e8ecee1c
NikkiE-Holberton <2495@holbertonschool.com>
2021-02-05 17:26:27 +0000

    Adding size file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main- This should display the size of the intergers
+* Return: 0
+*
+*/
+int main(void)
+{
+char = 1
+int = 2
+long int = 3
+long long int = 4
+float = 5
+
+printf("Size of a char: %c byte(s)\n (char))");
+printf("Size of a int: %d byte(s)\n (int))");
+printf("Size of a long int: %li byte(s)\n (long int))");
+printf("Size of a long long int:%lli byte(s)\n (long long int))");
+printf("Size of a float: %f byte(s)\n (float))");
+return (0);
+}
