005adf3fbb59725e71edb1bc01830f56795c7f88
NikkiE-Holberton <2495@holbertonschool.com>
2021-02-13 03:46:28 +0000

    Adding print num file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Number range from 0 -9
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	_putchar(i);
+
+	{
+		_putchar('\n');
+	}
+		return;
+}
