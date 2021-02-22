faec75f3f4e1cc399f3baf35ed460e25ab948c60
NikkiE-Holberton <2495@holbertonschool.com>
2021-02-22 00:42:42 +0000

    Adding strncat file and updating strcat file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 
 /**
- * char *_strcat - append the src string to the dest string,
+ * char *_strncat - append the src string to the dest string,
  * @src: Varuable
  * @dest: Variable
+ * @n: Interger
  * Return: value
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int x;
@@ -14,7 +15,7 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[i] != '\0')
 		i++;
-	for (x = 0 ; src[x] != '\0'; x++)
+	for (x = 0; src[x] != '\0' && n > x; x++)
 		dest[i + x] = src[x];
 
 	return (dest);
