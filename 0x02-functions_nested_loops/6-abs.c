0503d938a4c4bfe5582e7d64709f41da9e2772d9
NikkiE-Holberton <2495@holbertonschool.com>
2021-02-28 01:57:38 +0000

    Adding abs file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _abs - check the code for Holberton School students.
+ * @b: Digit
+ * Return: Always value
+ */
+int _abs(int b)
+{
+	if (b < 0)
+		return (-(b));
+	return (b);
+}
