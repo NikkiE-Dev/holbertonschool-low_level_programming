53eafbd4b9b48061ac6240999b40834403043c39
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-03 02:34:46 +0000

    Adding create array file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Value
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int x;
+	char *f;
+
+	f = malloc((size + 1) * sizeof(char));
+	if (f == NULL)
+	{
+		return (NULL);
+	}
+	x = 0;
+
+	while (x < size)
+	{
+		f[x] = c;
+		x++;
+	}
+	f[x] = '\0';
+	return (f);
+}
