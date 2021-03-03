597a0b8075d76800eac484b20b3f921cf862498a
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-03 21:38:33 +0000

    Updating concat file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,13 @@ char *str_concat(char *s1, char *s2)
 	int c;
 	char *result;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	for (a = 0; s1[a] != '\0'; a++)
 	{
