6b343b9a1778b3f2bb6b21b410e36f9228a246f7
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-03 05:26:49 +0000

    Adding strdup file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - check legnth of string.
+ * @s: Start of string
+ * Return: values
+ */
+int _strlen(char *s)
+{
+	int r = 0;
+
+	while (*(s + r) != '\0')
+	{
+		r++;
+	}
+	return (r);
+}
+/**
+ * *_strdup- sopying the string to newly allocated space
+ *
+ * @str: the string
+ *
+ * Return: Value
+ */
+char *_strdup(char *str)
+{
+	char *len = malloc(_strlen(str) + 1);
+	char *src = str;
+	char *dest = len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	while (*src != 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (len);
+}
