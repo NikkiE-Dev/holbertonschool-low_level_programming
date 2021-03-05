334d3f6e98d2ab6b90349435143635d30cb5dbcd
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-05 02:39:25 +0000

    Adding malloc check file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+
+/**
+ * malloc_checked - checked to see if there is sufficient memmory
+ * @b: Integer
+ * Return: Value
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+
+	return (ptr);
+}
