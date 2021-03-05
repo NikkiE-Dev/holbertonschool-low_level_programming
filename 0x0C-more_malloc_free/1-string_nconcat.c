d6156ba1fb3906a82b4288826c959fa827ea24b1
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-05 03:49:14 +0000

    Adding if else code to concat file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,11 +21,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (b = 0; s2[b] != '\0'; b++)
 	{
 	}
-		if (n < b)
+		if (n > b)
+		{
+			n = b;
+		}
+		else
 		{
 			b = n;
 		}
-
 
 	result = malloc((a + b + 1) * sizeof(char));
 	if (result == NULL)
