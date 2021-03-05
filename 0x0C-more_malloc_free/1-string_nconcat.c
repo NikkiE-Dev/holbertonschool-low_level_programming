e884c41470250b6400f73a85db8d30751d1f4739
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-05 03:41:41 +0000

    Adding concat file

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - combine s1 & s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: Amount of bytes in s2
+ * Return: Value
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int a;
+	unsigned int b;
+	unsigned int c;
+	char *result;
+
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+	}
+	for (b = 0; s2[b] != '\0'; b++)
+	{
+	}
+		if (n < b)
+		{
+			b = n;
+		}
+
+
+	result = malloc((a + b + 1) * sizeof(char));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; c < a; c++)
+	{
+		result[c] = s1[c];
+	}
+	for (c = a; c < a + b; c++)
+	{
+		result[c] = s2[c - a];
+	}
+	result[c] = '\0';
+	return (result);
+}
