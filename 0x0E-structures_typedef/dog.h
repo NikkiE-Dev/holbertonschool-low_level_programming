6f98889444cdae94116526a7ffdc0843c0747d82
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-10 04:42:25 +0000

    Add initial files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - stores info of dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ */
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
