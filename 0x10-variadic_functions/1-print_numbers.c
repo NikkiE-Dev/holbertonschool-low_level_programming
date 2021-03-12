a806d19fb93d4027637ef7a5616eac77980b40c4
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-12 04:51:56 +0000

    Updating separator null statement on print file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,6 +19,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
+
+		if (separator == NULL)
+			printf("%d", va_arg(ap, int));
 		if (i < n - 1)
 			printf("%d%s", va_arg(ap, int), separator);
 		else
