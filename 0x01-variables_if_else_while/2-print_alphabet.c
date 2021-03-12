f6a3881b660924c8d88630aec9f7444370ad66ff
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-12 23:05:17 +0000

    Adding alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int a;
+
+	a = 97;
+
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
