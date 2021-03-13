f568e7f3dc028377e37b55ef9fd1f235d56da9b7
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-13 00:44:31 +0000

    Adding backwards abc file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int a;
+
+	a = 122;
+
+	while (a > 96)
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
