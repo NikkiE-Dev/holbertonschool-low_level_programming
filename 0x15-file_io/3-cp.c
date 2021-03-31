fce626f90fa8891e230403599d7a6c406429fc46
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-31 22:21:41 +0000

    Updating 4th file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,8 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int opf_src, opf_dest, rdf, wtf, clf;
-	char buff[1024];
+	int opf_src, opf_dest, rdf = 1, wtf, clf;
+	char *buff[1024];
 
 	if (argc != 3)
 	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -34,16 +34,17 @@ int main(int argc, char *argv[])
 		{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(opf_src), close(opf_dest), exit(99); }
 	}
+
 	if (rdf == -1)
 	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
 	clf = close(opf_src);
 	if  (clf == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't close %d\n", opf_src);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", opf_src);
 		exit(100); }
 	clf = close(opf_dest);
 	if (clf == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't close %d\n", opf_dest);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", opf_dest);
 		exit(100); }
 	return (0);
 }
