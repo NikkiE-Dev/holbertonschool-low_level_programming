58a0456ff5c442c06c50da20d91c193507d04a34
NikkiE-Holberton <2495@holbertonschool.com>
2021-03-31 16:04:06 +0000

    Updating to include if text is null to 2nd file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,20 +10,31 @@ int create_file(const char *filename, char *text_content)
 {
 	int opf, wrf, sp = 0;
 
-
+/* Check to see if filename exist, if NULL return -1*/
 	if (filename == NULL)
 	{
 		return (-1);
 	}
+/*Create file with read & write permissions*/
+/*If file exist truncate it, and do not change permissions*/
 	opf = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+/*Check to see if creating file failed*/
 	if (opf == -1)
 	{
 		return (-1);
 	}
+/*If Text content = NULL just create an empty file*/
+	if (text_content == NULL)
+	{
+		close(opf);
+		return (1);
+	}
+/*Check the legnth of the text_context*/
 	while (text_content[sp] != '\0')
 		sp++;
-
+/*Write text content to file*/
 	wrf = write(opf, text_content, sp);
+/*Check to see if file was able to write if not return "fail"*/
 	if (wrf == -1)
 	{
 		write(STDOUT_FILENO, "fails", 6);
