b79dd27c0043cdc35a4d0c01e7d476b52c45dc90
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-28 23:06:38 +0000

    Making file betty compliant

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -4,19 +4,19 @@
  * hash_table_create - creates empty hash table
  *
  * @size: size of the array
- * 
+ *
  * Return: a pointer to the newly created hash table
- */ 
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *newtable = NULL;
+hash_table_t *newtable = NULL;
 
-    newtable = malloc(sizeof(hash_table_t));
-    if (!newtable)
-        return NULL;
-    newtable->array = malloc(sizeof(void *) * size);
-    if (!newtable->array)
-        return NULL;
-    newtable->size = size;
-    return (newtable);
+newtable = malloc(sizeof(hash_table_t));
+	if (!newtable)
+		return (NULL);
+newtable->array = malloc(sizeof(void *) * size);
+	if (!newtable->array)
+		return (NULL);
+newtable->size = size;
+return (newtable);
 }
