eebcf16137c65bdd90a2c519c4168e6e24e1fe7d
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-28 20:50:52 +0000

    Adding code to create empty hash table

---
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates empty hash table
+ *
+ * @size: size of the array
+ * 
+ * Return: a pointer to the newly created hash table
+ */ 
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *newtable = NULL;
+
+    newtable = malloc(sizeof(hash_table_t));
+    if (!newtable)
+        return NULL;
+    newtable->array = malloc(sizeof(void *) * size);
+    if (!newtable->array)
+        return NULL;
+    newtable->size = size;
+    return (newtable);
+}
